e5871745a1b9d01c5e576e92c41c7b522230fdcf
daimler <11229630+germanztz@users.noreply.github.com>
2024-08-27 16:50:33 +0200

    ollama cuda

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,11 +1,16 @@
 
 #stops a deployment for running
-microk8s kubectl -n private-gpt scale deployment private-gpt-ollama --replicas=0
+microk8s kubectl -n private-gpt scale deployment -l avena.engine=cuda --replicas=0
+microk8s kubectl -n private-gpt scale deployment -l avena.engine=cpu --replicas=0
 
 
 #get failed pods
 microk8s kubectl -n private-gpt get pods --field-selector=status.phase!=Running
 
 
-#rons a pod to execute a curl and then deletes the pod
+#runs a pod to execute a curl and then deletes the pod
 microk8s kubectl -n private-gpt run curl --image=radial/busyboxplus:curl -i --tty --rm
+
+#list microk8s images
+microk8s.ctr images list
+
